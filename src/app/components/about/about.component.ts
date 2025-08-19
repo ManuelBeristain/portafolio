64cd362a43cc659c6c5759818e5634d650ba26eb
ManuelBeristain <beristainpalacios@gmail.com>
2025-08-18 23:01:58 -0600

    PORTAFOLIO

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public subtitle2: string;
+  public email: string;
+
+  constructor() {
+    this.title = 'Manuel Beristain Palacios';
+    this.subtitle = 'Desarrollador Front End';
+    this.subtitle2 = 'Consultor de Sistemas'
+    this.email = 'beristainpalacios@gmail.com';
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
