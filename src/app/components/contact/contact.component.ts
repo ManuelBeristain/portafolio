dc275c61060f16d1a49527e2b4f01663ec27258b
ManuelBeristain <beristainpalacios@gmail.com>
2025-08-19 22:30:07 -0600

    PORTAFOLIO

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+public title: string;
+  public subtitle: string;
+  public subtitle2: string;
+  public email: string;
 
+  constructor() {
+    this.title = 'Manuel Beristain Palacios';
+    this.subtitle = 'Desarrollador Front End';
+    this.subtitle2 = 'Consultor de Sistemas'
+    this.email = 'beristainpalacios@gmail.com';
+  }
+
+  ngOnInit(): void {
+
+  }
 }
